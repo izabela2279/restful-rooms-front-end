6437421977fe937fba1c6f56482b1ed556d3c1c6
User Name <kauth333@gmail.com>
2023-02-04 13:54:14 -0500

    refactor NavBar with public and protected links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,25 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+// import styles from './NavBar.module.css'
+// import Logo from '../../assets/branding/logo.svg'
 
 const NavBar = ({ user, handleLogout }) => {
+
+  const publicLinks = (
+    <ul>
+      <li><NavLink to="/login">LOG IN</NavLink></li>
+      <li><NavLink to="/signup">SIGN UP</NavLink></li>
+    </ul>
+  )
+
+  const protectedLinks = (
+    <ul>
+      <li><NavLink to="/logout" onClick={handleLogout}>LOG OUT</NavLink></li>
+    </ul>
+  )
+
   return (
     <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><Link to="/profiles">Profiles</Link></li>
-          <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          <li><Link to="/change-password">Change Password</Link></li>
-        </ul>
-      :
-        <ul>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-        </ul>
-      }
+      {user ? protectedLinks : publicLinks}
     </nav>
   )
 }
