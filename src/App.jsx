f87df86c213c76b056b8434bde10aecb0d1fa7a5
User Name <kauth333@gmail.com>
2023-02-05 00:29:01 -0500

    stub up AllListing page component and import/ add route in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
+import AllListings from './pages/AllListings/AllListings'
 
 // components
 import NavBar from './components/NavBar/NavBar'
---
+++ src/pages/AllListings/AllListings.jsx
@@ -0,0 +1,11 @@
+import styles from './AllListings.module.css';
+
+const AllListings = (props) => {
+    return (
+        <main className ={styles.container}>
+            All Listings
+        </main>
+    )
+}
+
+export default AllListings
