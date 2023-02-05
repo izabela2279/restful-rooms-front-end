c5268d840856a190b5872e167d63e5b4405f9579
User Name <kauth333@gmail.com>
2023-02-05 18:21:18 -0500

    import usestatr, useEffect useParams and css to ListingDetails

--- src/pages/ListingsDetail/ListingDetails.jsx
+++ src/pages/ListingsDetail/ListingDetails.jsx
@@ -1,3 +1,9 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import styles from './BlogDetails.module.css'
+
+
+
 const ListingDetails = (props) => {
     return (
       <main>
