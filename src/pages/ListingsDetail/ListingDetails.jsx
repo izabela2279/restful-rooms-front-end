190f1200cc126ec2542e21352e19290a3813e04c
User Name <kauth333@gmail.com>
2023-02-05 18:23:55 -0500

    import ListngService in Listingdetails

--- src/pages/ListingsDetail/ListingDetails.jsx
+++ src/pages/ListingsDetail/ListingDetails.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import styles from './BlogDetails.module.css'
 
+//Services
+import * as listingService from "../../services/listingService"
+
 
 
 const ListingDetails = (props) => {
