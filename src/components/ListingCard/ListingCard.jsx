ba2dce9a97ec06d10a188e69068cfd8862dbe8d0
Izabela <izabelaszeremeta@gmail.com>
2023-02-06 09:05:47 -0600

    update

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -16,18 +16,8 @@ const ListingCard = ({ listing }) => {
           </span>
           {/* <AuthorInfo content={listing} /> */}
         </header>
-        <div className="form-group mb-4">
-					<label htmlFor="photo-upload" className="form-label">
-						Upload Photo
-					</label>
-					<input
-						type="file"
-						className="form-control"
-						id="photo-upload"
-						name="photo"
-						onChange={handleChangePhoto}
-					/>
-				</div> 
+       
+        
       </article>
     </Link>
   )
