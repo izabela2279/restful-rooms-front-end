e8e54fa2a09cbabfd702d8ef7d7d022e76d2576d
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-06 21:57:31 -0400

    Stub up ReviewCard

---
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,17 @@
+import ReviewCard from "../ReviewCard/ReviewCard"
+
+const Reviews = (props) => {
+  if (!props.reviews) return <h4>No Reviews</h4>
+
+  return (
+    <>
+      {props.reviews.map((review) => (
+        <ReviewCard
+          key={review._id}
+          review={review}
+          user={props.user}
+        />
+      ))}
+    </>
+  )
+}
