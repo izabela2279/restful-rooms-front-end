31846093a7c7bf52703751c80d5e74e522993fe5
Izabela <izabelaszeremeta@gmail.com>
2023-02-06 19:21:49 -0600

    updates

--- src/pages/ListingsDetail/ListingDetails.jsx
+++ src/pages/ListingsDetail/ListingDetails.jsx
@@ -52,14 +52,12 @@ const ListingDetails = (props) => {
 
           </span>
                 
-
-
-            </main>
-
             <section>
                 <h1>Review</h1>
                 <NewReview handleAddReview={handleAddReview} />
             </section>
+            
+            </main>
             <section>
                 <h1>Activities</h1>
                 <NewActivity handleAddActivity={handleAddActivity} />
