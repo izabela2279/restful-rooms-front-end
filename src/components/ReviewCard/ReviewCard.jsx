bacf641961b1d03867f30c559e0840fce5d89ed9
izabela2279 <114194904+izabela2279@users.noreply.github.com>
2023-02-09 16:23:31 -0600

    Update ReviewCard.jsx

--- src/components/ReviewCard/ReviewCard.jsx
+++ src/components/ReviewCard/ReviewCard.jsx
@@ -1,16 +1,13 @@
 import UserInfo from "../UserInfo/UserInfo"
 
 const ReviewCard = ({ review }) => {
-  console.log(review);
   return (
-    <>
     <article>
       <header>
         <UserInfo content={review} />
       </header>
       <p> {review.text}</p>
     </article>
-    </>
   )
 }
 
