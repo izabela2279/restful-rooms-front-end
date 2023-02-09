5260baaaedaabfeb329c68937f3c1e7f9d43f2fa
User Name <kauth333@gmail.com>
2023-02-08 20:05:38 -0500

    refactor container of listing card

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -3,8 +3,8 @@ import styles from './ListingCard.module.css'
 
 const ListingCard = ({ listing }) => {
   return (
-    <Link to={`/listings/${listing._id}`} style={{textDecoration: 'none', color: "black" }}>
-      <article  className={styles.container}>
+    <Link to={`/listings/${listing._id}`} style={{textDecoration: 'none', color: "black", padding:0 }}>
+      <article  className={styles.container} styles={{width:273}}>
        
           
            <img id='card-pic' src={listing.photo} style={{ objectFit:'cover',width: 277.33, height:263.45, borderRadius: 17 }} />
--- src/pages/ListingsDetail/ListingDetails.jsx
+++ src/pages/ListingsDetail/ListingDetails.jsx
@@ -47,7 +47,7 @@ const ListingDetails = (props) => {
             <main className ={styles.container} >
                 <h1>{listing.title}</h1>
                 <img alt="" src={listing.photo} />
-                <p>{listing.guests} guests . {listing.bedrooms} bedrooms . {listing.beds} beds . {listing.baths} baths</p>
+                <h2>{listing.guests} guests . {listing.bedrooms} bedrooms . {listing.beds} beds . {listing.baths} baths</h2>
                 <div className="resDev" style={{ display: 'flex', justifyContent:'space-between', gap:100 }}>
                 <div id='description'>
                 <p>{listing.description}</p>
