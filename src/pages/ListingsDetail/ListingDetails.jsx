f34ae312c4e11bf929034288c984d99d2b14e7bd
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-08 22:09:48 -0400

    Create handleDeleteReview

--- src/pages/ListingsDetail/ListingDetails.jsx
+++ src/pages/ListingsDetail/ListingDetails.jsx
@@ -38,6 +38,11 @@ const ListingDetails = (props) => {
         setListing({ ...listing, reservations: [...(listing.reservations || []), newReservation] })
     }
 
+    const handleDeleteReview = async (listingId, reviewId) => {
+        await listingService.deleteReview(listingId, reviewId)
+        setListing({ ...listing, reviews: listing.reviews.filter((c) => c._id !== reviewId) })
+    }
+
 
     console.log('Listing data', listing)
     if (!listing) return <h1>Loading</h1>
