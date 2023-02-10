362adcc4beed7d35b977064cf7dd03afdad67e4c
User Name <kauth333@gmail.com>
2023-02-10 11:30:40 -0500

    style landing and sign up button

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,4 +1,5 @@
 import style from './Landing.module.css'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   
@@ -7,7 +8,8 @@ const Landing = () => {
       <div id="welcome">
       <h1> A place where you can find your home away from home. 
       Offer and find your <b style={{color:"blue"}}>restfulRooms </b>here. </h1>
-
+      <h2>Newcomers start here..</h2>
+      <Link to="/signup"><button style={{ backgroundColor: "blue", width:300, color:"white", fontSize:20, borderRadius:20}}> Sign Up</button></Link>
     
       </div>
       <div id="landing-image">
