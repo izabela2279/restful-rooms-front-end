b9cbcfabc7a970849835560f30774ca632db2006
User Name <kauth333@gmail.com>
2023-02-09 20:50:06 -0500

    change avatar pic dimensions and nav bar links

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,13 +1,15 @@
+import styles from './UserInfo.module.css'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 const UserInfo = ({ content }) => {
   
   return (
-    <div>
+    <div className={styles.container}>
      
       {content.author.photo ?
-        <img src={content.author.photo} alt="The user's avatar" />:
+        <img style={{ width:50, height:50, objectFit:'cover', borderRadius: 50}}src={content.author.photo} alt="The user's avatar" />:
          <FontAwesomeIcon icon={faUser} />}
       <section>
         <h4>{content.author.name}</h4>
